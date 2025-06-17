ded1f3adbc87d41155d05cc637f5cf578e715218
Andre Bastian <152775162+andre4freelance@users.noreply.github.com>
2025-06-17 18:39:52 +0700

    Update setup-git.sh

--- setup-git.sh
+++ setup-git.sh
@@ -4,5 +4,5 @@ ssh -T git@github.com
 
 
 
-git config --global user.email "andre4freelance@gmail.com"
-git config --global user.name "andre4freelance"
+git config --global user.email "user@mail,com"
+git config --global user.name "username"
